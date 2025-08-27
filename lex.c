e4cc123d1e2e4057d161e52faebdac10ab11e55e
Adrien Heisch <aheisch@student.42.fr>
2025-08-27 14:27:26 +0200

    fix heredoc lexing

--- lex.c
+++ lex.c
@@ -85,13 +85,13 @@ static t_token	*get_token(t_string *str, size_t *idx)
 		token.data.redir.type = REDIR_IN;
 		token.data.redir.fd = 0;
 		(*idx)++;
-		while (str->content[*idx] == ' ')
-			(*idx)++;
 		if (str->content[*idx] == '<')
 		{
 			token.data.redir.type = REDIR_HEREDOC;
 			(*idx)++;
 		}
+		while (str->content[*idx] == ' ')
+			(*idx)++;
 		if (!is_arg(str->content[*idx]))
 			return (errno = 1, NULL);
 		token.data.redir.file_name = parse_arg(str, idx);
