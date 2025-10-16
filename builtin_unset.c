16b6cae286598d98fac8beb9eb324c3a3f86cf31
Adrien Heisch <aheisch@student.42.fr>
2025-10-16 17:13:24 +0200

    fix unset export list

--- builtin_unset.c
+++ builtin_unset.c
@@ -11,6 +11,48 @@
 /* ************************************************************************** */
 
 #include "minishell.h"
+#include <stdlib.h>
+
+static void	remove_export_loop(size_t len, char *name, char **new, char **old)
+{
+	size_t	idx;
+	size_t	n_skipped;
+
+	idx = 0;
+	n_skipped = 0;
+	while (idx + n_skipped < len)
+	{
+		if (ft_strncmp(old[idx + n_skipped], name,
+				ft_strlen(old[idx + n_skipped])) == 0)
+		{
+			free(old[idx + n_skipped]);
+			n_skipped++;
+			continue ;
+		}
+		new[idx] = old[idx + n_skipped];
+		idx++;
+	}
+	new[idx] = NULL;
+}
+
+static t_err remove_export(char ***exported, char *name)
+{
+	char	**old;
+	char	**new;
+	size_t	len;
+
+	old = *exported;
+	len = 0;
+	while (old[len])
+		len++;
+	new = malloc(len * sizeof(char *));
+	if (!new)
+		return (ERR_SYSTEM);
+	remove_export_loop(len, name, new, old);
+	free(old);
+	*exported = new;
+	return (ERR_OK);
+}
 
 t_err	builtin_unset(t_exec_info *cmd, t_shell_data *shell_data)
 {
@@ -23,6 +65,8 @@ t_err	builtin_unset(t_exec_info *cmd, t_shell_data *shell_data)
 	while (cmd->args[idx])
 	{
 		if (ft_unsetenv(&shell_data->envp, cmd->args[idx]))
+			return (ERR_SYSTEM);
+		if (remove_export(&shell_data->exported, cmd->args[idx]))
 			return (ERR_SYSTEM);
 		idx++;
 	}
